c36f18353b91283c6fdf0618791f1ff6f56b3add
Tiago Barufi <barufi@gmail.com>
2016-11-01 12:17:02 -0200

    imagens

--- main.py
+++ main.py
@@ -14,6 +14,10 @@ def index():
 def send_js(path):
     return send_from_directory('js', path)
 
+@app.route('/images/<path:path>')
+def send_images(path):
+    return send_from_directory('images', path)
+
 @app.route('/css/<path:path>')
 def send_css(path):
     return send_from_directory('css', path)
@@ -73,14 +77,23 @@ def reverse():
     p=request.values.getlist('point[]')
     conn = psycopg2.connect("dbname='bigrs' user='bigrs' host='localhost' port='5433' password='bigrs'")
     cur = conn.cursor()
-    print(p)
     cur.execute("select bigrs.reverse_geocode(%s, %s)", (p[0],p[1],))
     r=cur.fetchone()
     return r[0]
 @app.route('/report',methods=['GET','POST'])
 def report():
+    p=request.values.get('codlog')
+    h = {'codlog':p}
+    conn = psycopg2.connect("dbname='bigrs' user='bigrs' host='localhost' port='5433' password='bigrs'")
+    cur = conn.cursor()
+    cur.execute("select lg_total_length from logradouro_details where lg_codlog=%s", (p,))
+    r=cur.fetchone()
+    h['length']=round(float(r[0]))
+    cur.execute("select * from incidentes where st_contains(st_transform((select st_buffer(st_union(geom),5 0) from sirgas_shp_logradouro where lg_codlog=%s),32723), geom)='t'", (p,))
 
-    return ""
+    r = cur.fetchall()
+    print(r)
+    return render_template('report.html',**h)
 
 
 app.run(host='0.0.0.0',debug=True)
