c55816a0508ec553d85b41ab0b6a5ade80dad821
Tiago Barufi <barufi@gmail.com>
2017-04-11 18:22:35 -0300

    mapas desenhando

--- bigrs/static/js/admin/mapinha.js
+++ bigrs/static/js/admin/mapinha.js
@@ -15,6 +15,9 @@ console.debug(tr);
         }
     }});
 }
+
+
+
 $(document).ready(function(){
     latlng=MAP_CENTER.match(/(-?[\d\.]+) (-?[\d\.]+)/);
     var format = 'image/png';
@@ -46,11 +49,11 @@ $(document).ready(function(){
             0.14929107084870338
         ]
     });
-map.isValidZoomLevel = function(zoomLevel) {
-   return ( (zoomLevel != null) &&
-      (zoomLevel >= 17) && // set min level here, could read from property
-      (zoomLevel < 22 ));
-}
+    map.isValidZoomLevel = function(zoomLevel) {
+       return ( (zoomLevel != null) &&
+          (zoomLevel >= 17) && // set min level here, could read from property
+          (zoomLevel < 22 ));
+    }
     map.addLayer(tiled);
     zoom=17;
 
@@ -83,6 +86,8 @@ map.isValidZoomLevel = function(zoomLevel) {
             }})
         });
     map.addLayer(vectors);
+    var arrowheads = new OpenLayers.Layer.Vector( "Heads" );
+    map.addLayer(arrowheads);
     var i=0;
     features=[];
     while($("#spot_set-"+i).length){
@@ -101,7 +106,7 @@ map.isValidZoomLevel = function(zoomLevel) {
     }
     vectors.addFeatures(features);
 
-    dc=new OpenLayers.Control.DrawFeature(vectors, OpenLayers.Handler.Point);
+    dc=new OpenLayers.Control.DrawFeature(vectors, OpenLayers.Handler.Path);
     map.addControl(dc);
     dc.activate();
     if(!latlng)
@@ -141,3 +146,64 @@ map.isValidZoomLevel = function(zoomLevel) {
 
 });
 })(django.jQuery);
+function drawHeads(){
+    var feat=[];
+    for(var i=0;i<vectors.features.length;i++){
+        var v=vectors.features[i];
+        if(v.geometry && v.geometry.components && (v.geometry.components.length>1)){
+            var p=[v.geometry.components[v.geometry.components.length-2],v.geometry.components[v.geometry.components.length-1]];
+            console.debug(p);
+            var m=(p[1].y-p[0].y)/(p[1].x-p[0].x);
+
+
+            var modulus=Math.sqrt(Math.pow(p[1].y-p[0].y,2.0)+Math.pow(p[1].x-p[0].x,2.0));
+            console.debug(modulus);
+
+            var size=10;
+            var dx=10*(p[1].x-p[0].x)/modulus;
+            var dy=10*(p[1].y-p[0].y)/modulus;
+            console.debug(dx);
+            console.debug(dy);
+            var a=[p[1].x-dx,p[1].y-dy];
+            var mu=1
+            var avb=[dy*mu,-1*dx*mu];
+            mu=-1;
+            var amb=[dy*mu,-1*dx*mu]
+
+            var b=[a[0]+amb[0],a[1]+amb[1]];
+            var c=[a[0]+avb[0],a[1]+avb[1]];
+
+
+console.debug([dx,dy]);
+console.debug(avb);
+console.debug(amb);
+
+            feat.push(new OpenLayers.Feature.Vector(
+                new OpenLayers.Geometry.Point(
+                    p[1].x-dx,p[1].y-dy
+                ), {
+                    type: 5 + parseInt(5 * Math.random()),
+                    name:i+1
+                })
+             );
+             feat.push(new OpenLayers.Feature.Vector(
+                new OpenLayers.Geometry.Point(
+                    b[0],b[1]
+                ), {
+                    type: 5 + parseInt(5 * Math.random()),
+                    name:i+1
+                })
+             );
+             feat.push(new OpenLayers.Feature.Vector(
+                new OpenLayers.Geometry.Point(
+                    c[0],c[1]
+                ), {
+                    type: 5 + parseInt(5 * Math.random()),
+                    name:i+1
+                })
+             );
+        }
+    }
+    var arrowheads=map.getLayersByName("Heads")[0];
+    arrowheads.addFeatures(feat);
+}
